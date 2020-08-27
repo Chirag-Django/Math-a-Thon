8d4778bc2f30d52775a8abb4aab4660082f86214
CKdave <chirag.django@gmail.com>
2020-08-27 06:03:40 +0530

    tweaked again for heroku

--- mathathon/settings.py
+++ mathathon/settings.py
@@ -124,11 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
